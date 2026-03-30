be151f68db848e798cd651f229c2f3d827ff3262
Aaron <projectgame@163.com>
2026-03-30 11:41:20 +0900

    modify clear icon badge

--- fundex/android/app/src/main/kotlin/com/fundex/app/MainActivity.kt
+++ fundex/android/app/src/main/kotlin/com/fundex/app/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.fund.stellavia
 
+import android.app.NotificationManager
+import android.content.Context
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onResume() {
+        super.onResume()
+        clearNotifications()
+    }
+
+    private fun clearNotifications() {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
+        notificationManager?.cancelAll()
+    }
+}
--- fundex/ios/Runner/AppDelegate.swift
+++ fundex/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import UserNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,10 +8,24 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    clearApplicationBadge(application)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func applicationDidBecomeActive(_ application: UIApplication) {
+    super.applicationDidBecomeActive(application)
+    clearApplicationBadge(application)
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+
+  private func clearApplicationBadge(_ application: UIApplication) {
+    application.applicationIconBadgeNumber = 0
+    UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+    if #available(iOS 16.0, *) {
+      UNUserNotificationCenter.current().setBadgeCount(0) { _ in }
+    }
   }
 }
