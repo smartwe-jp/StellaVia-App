efb29d5133c1e387254b8c875bfbdff377f796fc
Aaron <projectgame@163.com>
2026-04-07 15:08:56 +0900

    modify badge clear

--- fundex/ios/Runner/AppDelegate.swift
+++ fundex/ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import UserNotifications
+import CloudPushSDK
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -8,6 +9,7 @@ import UserNotifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    installBadgeResetObservers()
     clearApplicationBadge(application)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
@@ -21,11 +23,45 @@ import UserNotifications
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 
+  private func installBadgeResetObservers() {
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(handleBadgeResetLifecycleEvent),
+      name: UIApplication.didBecomeActiveNotification,
+      object: nil
+    )
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(handleBadgeResetLifecycleEvent),
+      name: UIApplication.willEnterForegroundNotification,
+      object: nil
+    )
+    if #available(iOS 13.0, *) {
+      NotificationCenter.default.addObserver(
+        self,
+        selector: #selector(handleBadgeResetLifecycleEvent),
+        name: UIScene.didActivateNotification,
+        object: nil
+      )
+      NotificationCenter.default.addObserver(
+        self,
+        selector: #selector(handleBadgeResetLifecycleEvent),
+        name: UIScene.willEnterForegroundNotification,
+        object: nil
+      )
+    }
+  }
+
+  @objc private func handleBadgeResetLifecycleEvent() {
+    clearApplicationBadge(UIApplication.shared)
+  }
+
   private func clearApplicationBadge(_ application: UIApplication) {
     application.applicationIconBadgeNumber = 0
     UNUserNotificationCenter.current().removeAllDeliveredNotifications()
     if #available(iOS 16.0, *) {
       UNUserNotificationCenter.current().setBadgeCount(0) { _ in }
     }
+    CloudPushSDK.syncBadgeNum(0) { _ in }
   }
 }
